902bdd06e0c618cb21a52c8b4b523c840baafd81
Tamal Biswas <tamalbiswas32@gmail.com>
2023-07-31 15:04:19 +0530

    Afternoon upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        nunito: ["var(--font-nunito)", ...defaultTheme.fontFamily.sans],
+        lato: ["var(--font-lato)", ...defaultTheme.fontFamily.serif],
+        kalam: ["var(--font-kalam)", ...defaultTheme.fontFamily.cursive],
       },
     },
   },
   plugins: [],
-}
+};
